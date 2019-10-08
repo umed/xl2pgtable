b154e9a149d7db083fb41322efb01811a454f767
Umedzhon Abdumuminov <ovegug@gmail.com>
2019-10-08 10:57:17 +0300

    Added test

--- tests/test_database_utils.py
+++ tests/test_database_utils.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 from uploader.database.database_utils import null_or_format_str, py_value_to_pg_value
 from uploader.utils import NULL
 
@@ -8,3 +10,16 @@ def test_single_quote_escape():
 
 def test_single_space_int():
     assert py_value_to_pg_value(int, ' ') == NULL
+
+
+def test_mapping():
+    value_type = {
+        'mapping': {
+            'type': 'interval'
+        }
+    }
+
+    value_str = '12.12.2014 13:53:03'
+    value_format = '%d.%m.%Y %H:%M:%S'
+    value = dt.datetime.strptime(value_str, value_format)
+    assert py_value_to_pg_value(value_type, value) == "to_timestamp('{}', 'dd.mm.yyyy hh24:mi:ss')".format(value_str)
